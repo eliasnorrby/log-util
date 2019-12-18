95fb9a98baaa23719d96e3dcbeed042cec92f47d
Elias Norrby <elias.norrby@gmail.com>
2019-12-18 15:16:50 +0100

    test: add a dead simple test

---
+++ index.test.js
@@ -0,0 +1,23 @@
+const log = require("./index");
+
+describe("log", () => {
+  it("should have an 'ok' method", () => {
+    log.ok("I am okay");
+  });
+
+  it("should have an 'info' method", () => {
+    log.info("Stay informed");
+  });
+
+  it("should have a 'skip' method", () => {
+    log.skip("Skipping ahead");
+  });
+
+  it("should have a 'warn' method", () => {
+    log.warn("Be warned");
+  });
+
+  it("should have a 'fail' method", () => {
+    log.fail("Do not fail me");
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require("@eliasnorrby/jest-config"),
+  roots: ["."]
+};
